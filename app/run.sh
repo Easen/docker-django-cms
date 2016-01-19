84bc080f7200d5ea9c8fcbb7bc69cf0454935db2
Marc Easen <marc@easen.co.uk>
2016-01-19 16:44:30 +0000

    Moved the run.sh into the app folder

--- app/run.sh
+++ app/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 NEW_SECRET=$(LC_CTYPE=C tr -dc A-Za-z0-9 < /dev/urandom | fold -w ${1:-32} | head -n 1)
-sed -i "s/##YOUR_SECRET_KEY##/${NEW_SECRET}/g" app/mysite/settings.py
+sed -i "s/##YOUR_SECRET_KEY##/${NEW_SECRET}/g" mysite/settings.py
 
-cd app
 python manage.py runserver 0.0.0.0:8000
 
